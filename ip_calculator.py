c8568b5d5b20fb7abf3b2c5d0c051e32d4d15238
kevincogan <44865890+kevincogan@users.noreply.github.com>
2020-10-14 17:22:18 +0100

    Finish with out refactoring

--- ip_calculator.py
+++ ip_calculator.py
@@ -117,33 +117,54 @@ def get_CIDR(subnet_mask_address): #converts the string to binary the counts the
     return str(("".join(subnet_mask_address)).count("1"))
 
 def get_subnets(subnet_mask, network_class):
+
     if network_class == "C":
-        return str(2 ** subnet_mask[1].count("1"))
+        return str(2 ** subnet_mask[2].count("1"))
 
     elif network_class == "B":
-        return str(2 ** "".join(subnet_mask[0:2]).count("1"))
+        return str(2 ** "".join(subnet_mask[1:3]).count("1"))
+
+    elif network_class == "A":
+        return str(2 ** "".join(subnet_mask[0:3]).count("1"))
 
 
 def get_host_per_subnet(subnet_mask, network_class):
     if network_class == "C":
-        return str(2 ** subnet_mask[1].count("0") - 2)
+        return str(2 ** subnet_mask[2].count("0") - 2)
 
     elif network_class == "B":
-        return str(2 ** "".join(subnet_mask[0:2]).count("0") - 2)
+        return str(2 ** "".join(subnet_mask[1:3]).count("0") - 2)
 
-def get_valid_subnet(ip_addr_bytes, number_of_hosts, network_class):
-    valid_subnet_list = [".".join(ip_addr_bytes)]
+    elif network_class == "A":
+        return str(2 ** "".join(subnet_mask[0:3]).count("0") - 2)
+
+def get_valid_subnet(ip_addr_bytes, subnet, network_class, subnet_mask):
     counter = 0
 
-    while counter != 192:
-        counter = counter + 64
-        if network_class == "C":
-            new_byte = ".".join(ip_addr_bytes[0:3]) + "." + str(counter)
-            valid_subnet_list.append(new_byte)
+    if network_class == "C":
+        byte_limit = (subnet_mask.split("."))[3]
+        prefix = ""
+        index = 3
 
-        elif network_class == "B":
-            new_byte = ".".join(ip_addr_bytes[0:2]) + "." + str(counter) + "." + ip_addr_bytes[3]
-            valid_subnet_list.append(new_byte)
+    elif network_class == "B":
+        byte_limit = (subnet_mask.split("."))[2]
+        prefix = "." + ip_addr_bytes[3]
+        index = 2
+
+    elif network_class == "A":
+        ip_addr_bytes[1] = "0"
+        byte_limit = (subnet_mask.split("."))[1]
+        prefix = "." + ip_addr_bytes[2] +"." + ip_addr_bytes[3]
+        index = 1
+
+    valid_subnet_list = [".".join(ip_addr_bytes)]
+    while counter != int(byte_limit):
+        counter = counter + (256 // int(subnet))
+        new_byte = ".".join(ip_addr_bytes[0:index]) + "." + str(counter) + prefix
+        valid_subnet_list.append(new_byte)
+
+        #new_byte = ".".join(ip_addr_bytes[0:2]) + "." + str(counter) + "." + ip_addr_bytes[3]
+        #valid_subnet_list.append(new_byte)
 
     return valid_subnet_list
 
@@ -152,15 +173,23 @@ def get_broadcasting_address(valid_subnet, ip_addr_bytes, network_class):
     broadcast_list = []
     for address in valid_subnet[1:4]:
         address = address.split(".")
+
         if network_class == "C":
             broadcast_list.append(".".join(address[0:3])+ "." + str(int(address[3]) - 1))
-            byte = address[2]
+            index = 3
+            postfix = ".255"
 
         elif network_class == "B":
             broadcast_list.append(".".join(address[0:2])+ "." + str(int(address[2]) - 1) + "." + "255")
-            byte = 255
+            index = 2
+            postfix = ".255" * 2
 
-    broadcast_list.append(".".join(ip_addr_bytes[0:2]) + "." + str(byte) + "." + "255")
+        elif network_class == "A":
+            broadcast_list.append(".".join(address[0:1])+ "." + str(int(address[1]) - 1) + ".255" * 2)
+            index = 1
+            postfix = ".255" * 3
+
+    broadcast_list.append(".".join(ip_addr_bytes[0:index]) + postfix)
     return broadcast_list
 
 
@@ -172,27 +201,62 @@ def get_last_address(broadcast_address):
     first_address = [".".join(byte.split(".")[0:3]) + "." + str(int(byte.split(".")[3]) - 1) for byte in broadcast_address]
     return first_address
 
+def common_bytes(addresses):
+    i = 0
+    common_bytes = []
+    flag = True
+    binary_addresses = [to_binary_string(address) for address in addresses]
+    binary_addresses = ["".join(address) for address in binary_addresses]
+    while i != len(binary_addresses[0]) and flag == True:
+        if binary_addresses[0][i] == binary_addresses[1][i] and binary_addresses[2][i] == binary_addresses[3][i] and binary_addresses[1][i] == binary_addresses[2][i] and binary_addresses[0][i] == binary_addresses[3][i]:
+            common_bytes.append(binary_addresses[0][i])
+            flag = True
+        else:
+            flag = False
+        i += 1
 
+    return "".join(common_bytes)
+
+def get_network_mask(same_bytes):
+    i = 0
+    post_fix_bits = 32 - len(same_bytes)
+    network_mask = "1" * len(same_bytes) + ("0" * post_fix_bits)
+
+    network_mask_list = [network_mask[0:8], network_mask[8:16], network_mask[16:24], network_mask[24:32]]
+    return to_decimal_dot(network_mask_list)
+
+#################################################################################
+#def get_supernet_stats(addresses):
+#    same_bytes = common_bytes(addresses)
+#    print("Address: " + addresses[0] + "/" + str(len(same_bytes)))
+#
+#    network_mask = get_network_mask(same_bytes)
+#    print("Network: " + network_mask)
+#
+#
+#get_supernet_stats(["205.100.0.0","205.100.1.0","205.100.2.0","205.100.3.0"])
 #################################################################################
 def get_subnet_stats(ip_addr,subnet_mask):
     ip_addr_bytes = ip_addr.split(".")
     subnet_mask_address = to_binary_string(subnet_mask)
     network_class = (get_network_class(ip_addr_bytes[0]))
 
+    print(network_class)
+
     #Gets the CIDR notation and adds it to the end of the IP address.
     cidr_notation = get_CIDR(subnet_mask_address)
     print("Address: " + ip_addr + "/" + cidr_notation)
 
     #Calculating the subnets.
-    subnets = get_subnets(subnet_mask_address[2:4], network_class)
+    subnets = get_subnets(subnet_mask_address[1:4], network_class)
     print("Subnets: " + subnets)
 
     #Calculating the addressable hosts per subnet.
-    number_of_hosts = get_host_per_subnet(subnet_mask_address[2:4], network_class)
+    number_of_hosts = get_host_per_subnet(subnet_mask_address[1:4], network_class)
     print("Addressable hosts per subnet: " + number_of_hosts)
 
     #Creating the valid subnet subnet_mask_address.
-    valid_subnet = get_valid_subnet(ip_addr_bytes, number_of_hosts, network_class)
+    valid_subnet = get_valid_subnet(ip_addr_bytes, subnets, network_class,subnet_mask)
     print("Valid subnets: " + str(valid_subnet))
 
     #Get the valid Broafcasting addresses
@@ -203,14 +267,10 @@ def get_subnet_stats(ip_addr,subnet_mask):
     print("First addresses: " + str(first_address))
 
     last_address = get_last_address(broadcast_address)
-    print("Broadcast addresses: " + str(last_address))
+    print("Last addresses: " + str(last_address))
 
-
-
-
-
-
-get_subnet_stats("172.16.0.0","255.255.192.0")
+get_subnet_stats("127.16.0.0", "255.192.0.0")
+#get_subnet_stats("172.16.0.0","255.255.192.0")
 #get_subnet_stats("192.168.10.0","255.255.255.192")
 #################################################################################
 #def get_class_stats(ip_addr):
