8d8502b19cf23c0e32de7afeac03711c758c9307
kevincogan <44865890+kevincogan@users.noreply.github.com>
2020-10-24 11:57:18 +0100

    Finished

--- ip_calculator.py
+++ ip_calculator.py
@@ -1,3 +1,4 @@
+#Dictionary containing attributes speciffic to each Class.
 classes={
     'A':{
         'network_bits':7,
@@ -28,7 +29,7 @@ classes={
         'network_bits':'N/A',
         'host_bits':'N/A',
         'first_address': "240" + (".0" * 3),
-        'last_address': "225" + (".255" * 3)
+        'last_address': "255" + (".255" * 3)
         },
         }
 
@@ -38,7 +39,6 @@ def to_binary_string(ip_addr):
     # convert each number into a int, format it as binary, turn it back intoa stirng
     # and return it as an array, isn't python great !
     return ['{0:08b}'.format(int(x)) for x in byte_split]
-
 
 
 def to_decimal_dot(ip_addr_list):
@@ -77,15 +77,17 @@ def get_network_class(byte): #The first eight bits of the network address will i
 
     return class_type #returns a string eith the class of the inputted network.
 
+
 #Converts the string to binary the counts the number of ones in the binary string to get the CIDR notation.
 def get_CIDR(subnet_mask_address):
     return str(("".join(subnet_mask_address)).count("1")) #Counts and retuns the number of ones.
+
 
 #Calcuates the number of subnets by counting the number of ones in the host bytes and putting that value over the power of two.
 def get_subnets(subnet_mask, network_class):
 
     if network_class == "C":
-        return str(2 ** subnet_mask[2:3].count("1")) #Gets the class C host bytes, counts the ones, and puts the value over the power of two.
+        return str(2 ** "".join(subnet_mask[2:3]).count("1")) #Gets the class C host bytes, counts the ones, and puts the value over the power of two.
 
     elif network_class == "B":
         return str(2 ** "".join(subnet_mask[1:3]).count("1")) #Gets the class B host bytes, counts the ones, and puts the value over the power of two.
@@ -93,12 +95,13 @@ def get_subnets(subnet_mask, network_class):
     elif network_class == "A":
         return str(2 ** "".join(subnet_mask[0:3]).count("1")) #Gets the class A host bytes, counts the ones, and puts the value over the power of two.
 
+
 #This calculates of addressable host for each class
 def get_host_per_subnet(subnet_mask, network_class):
 
     #Calculates the the addressable host for class C by counting all the zeros in the host bytes, putting that value to the power of two, then substracting 2.
     if network_class == "C":
-        return str(2 ** subnet_mask[2:3].count("0") - 2)
+        return str(2 ** "".join(subnet_mask[2:3]).count("0") - 2)
 
     #Calculates the the addressable host for class B by counting all the zeros in the host bytes, putting that value to the power of two, then substracting 2.
     elif network_class == "B":
@@ -107,6 +110,7 @@ def get_host_per_subnet(subnet_mask, network_class):
     #Calculates the the addressable host for class A by counting all the zeros in the host bytes, putting that value to the power of two, then substracting 2.
     elif network_class == "A":
         return str(2 ** "".join(subnet_mask[0:3]).count("0") - 2)
+
 
 #Gets all the valid subnets for Class A, B, and C.
 def get_valid_subnet(ip_addr_bytes, network_class, subnet_mask):
@@ -137,6 +141,7 @@ def get_valid_subnet(ip_addr_bytes, network_class, subnet_mask):
 
     return valid_subnet_list
 
+
 #Gets a list of broadcasting addresses for the IP addresses
 def get_broadcasting_address(valid_subnet, ip_addr_bytes, network_class):
     broadcast_list = [] #Contains all broadcasting address.
@@ -163,15 +168,18 @@ def get_broadcasting_address(valid_subnet, ip_addr_bytes, network_class):
     broadcast_list.append(".".join(ip_addr_bytes[0:index]) + postfix) #creates the last broadcasting address and adds it to the broadcast list.
     return broadcast_list
 
+
 #Finds the first address of the IP address.
 def get_first_address(valid_subnet):
     first_address = [".".join(byte.split(".")[0:3]) + "." + str(int(byte.split(".")[3]) + 1) for byte in valid_subnet] #The valid subnet list is taken and one bit is added to the last byte of every subnet address to make the first address.
     return first_address
 
+
 #Finds the last address of the IP address.
 def get_last_address(broadcast_address):
     last_address = [".".join(byte.split(".")[0:3]) + "." + str(int(byte.split(".")[3]) - 1) for byte in broadcast_address] #The broadcast address is used and one bit is substracted from the last byte of every address
     return last_address
+
 
 #finds all the bytes that the addresses have in common.
 def common_bytes(addresses):
@@ -192,6 +200,7 @@ def common_bytes(addresses):
 
     return "".join(common_bytes) #Join all the common bits in the list and return.
 
+
 #This will make the network mask for the fiven addresses.
 def get_network_mask(same_bytes):
     post_fix_bits = 32 - len(same_bytes) #Calcuates the number of zeros in the binary subnet mask.
@@ -200,73 +209,82 @@ def get_network_mask(same_bytes):
     network_mask_list = [network_mask[0:8], network_mask[8:16], network_mask[16:24], network_mask[24:32]] #Formates the network mask into a list with sublist to be converted into decimal dot notation.
     return to_decimal_dot(network_mask_list) #Converts binary list to decimal dot notation.
 
-#################################################################################
-#def get_supernet_stats(addresses):
-#    #Gets a list of the same bytes from the inputed addresses.
-#    same_bytes = common_bytes(addresses)
-#    print("Address: " + addresses[0] + "/" + str(len(same_bytes))) #Formatting onto an address and finding the length of the common bits for to create CIDR Notation.
-#
-#    #retrieves the network mask.
-#    network_mask = get_network_mask(same_bytes)
-#    print("Network: " + network_mask) #Formatting for output.
-#
-#
+#SUPERNETTING MODULE.
+def get_supernet_stats(addresses):
+   #Gets a list of the same bytes from the inputed addresses.
+   same_bytes = common_bytes(addresses)
+   print("Address: " + addresses[0] + "/" + str(len(same_bytes))) #Formatting onto an address and finding the length of the common bits for to create CIDR Notation.
+
+   #retrieves the network mask.
+   network_mask = get_network_mask(same_bytes)
+   print("Network: " + network_mask) #Formatting for output.
+
+
 #get_supernet_stats(["205.100.0.0","205.100.1.0","205.100.2.0","205.100.3.0"])
-#################################################################################
-#def get_subnet_stats(ip_addr,subnet_mask):
-#    ip_addr_bytes = ip_addr.split(".")
-#    subnet_mask_address = to_binary_string(subnet_mask)
-#    network_class = (get_network_class(ip_addr_bytes[0]))
-#
-#    #print(network_class)
-#
-#    #Gets the CIDR notation and adds it to the end of the IP address.
-#    cidr_notation = get_CIDR(subnet_mask_address)
-#    print("Address: " + ip_addr + "/" + cidr_notation)
-#
-#    #Calculating the subnets.
-#    subnets = get_subnets(subnet_mask_address[1:4], network_class)
-#    print("Subnets: " + subnets)
-#
-#    #Calculating the addressable hosts per subnet.
-#    number_of_hosts = get_host_per_subnet(subnet_mask_address[1:4], network_class)
-#    print("Addressable hosts per subnet: " + number_of_hosts)
-#
-#    #Creating the valid subnet subnet_mask_address.
-#    valid_subnet = get_valid_subnet(ip_addr_bytes, network_class,subnet_mask)
-#    print("Valid subnets: " + str(valid_subnet))
-#
-#    #Get the valid Broafcasting addresses
-#    broadcast_address = get_broadcasting_address(valid_subnet, ip_addr_bytes, network_class)
-#    print("Broadcast addresses: " + str(broadcast_address))
-#
-#    first_address = get_first_address(valid_subnet)
-#    print("First addresses: " + str(first_address))
-#
-#    last_address = get_last_address(broadcast_address)
-#    print("Last addresses: " + str(last_address))
-#
+
+
+#SUBNETTING MODULE.
+def get_subnet_stats(ip_addr,subnet_mask):
+   ip_addr_bytes = ip_addr.split(".")
+   subnet_mask_address = to_binary_string(subnet_mask)
+   network_class = (get_network_class(ip_addr_bytes[0]))
+
+   #Gets the CIDR notation and adds it to the end of the IP address.
+   cidr_notation = get_CIDR(subnet_mask_address)
+   print("Address: {}/{}".format(ip_addr, cidr_notation))
+
+   #Calculating the subnets.
+   subnets = get_subnets(subnet_mask_address[1:4], network_class)
+   print("Subnets: {}".format(subnets))
+
+   #Calculating the addressable hosts per subnet.
+   number_of_hosts = get_host_per_subnet(subnet_mask_address[1:4], network_class)
+   print("Addressable hosts per subnet: {}".format(number_of_hosts))
+
+   #Creating the valid subnet subnet_mask_address.
+   valid_subnet = get_valid_subnet(ip_addr_bytes, network_class,subnet_mask)
+   print("Valid subnets: {}".format(str(valid_subnet)))
+
+   #Get the valid Broadcasting addresses
+   broadcast_address = get_broadcasting_address(valid_subnet, ip_addr_bytes, network_class)
+   print("Broadcast addresses: {}".format(str(broadcast_address)))
+
+   #Gets the first address
+   first_address = get_first_address(valid_subnet)
+   print("First addresses: {}".format(str(first_address)))
+
+   #Gets the last address.
+   last_address = get_last_address(broadcast_address)
+   print("Last addresses: {}".format(str(last_address)))
+
 #get_subnet_stats("127.16.0.0", "255.192.0.0")
 #get_subnet_stats("172.16.0.0","255.255.192.0")
 #get_subnet_stats("192.168.10.0","255.255.255.192")
-#################################################################################
+
+
+#CLASS MODULE.
 def get_class_stats(ip_addr):
     each_byte = ip_addr.split(".") #Slipt the eight bits into a list.
 
-    #Finds the clas of the network inputted and store it in a variable so it can be used in other tasks.
+    #Finds the clas of the network inputted and stores it in a variable so it can be used in other tasks.
     network_class = get_network_class(each_byte[0])
     print("Class: " + network_class)
 
+    #Determines if the class is less than C and then retrieves the information from the dictionary and formats it to be output.
     if network_class <= "C":
         print("Network: " + str(2 ** classes[network_class]["network_bits"]))
         print("Host: " + str(2 ** classes[network_class]['host_bits']))
 
+    #Determines the class is D or E and then retrieves the information from the dictionary and formats it to be output.
     elif network_class == "D" or network_class == "E":
         print("Network: " + classes[network_class]["network_bits"])
         print("Host: " + classes[network_class]['host_bits'])
 
+    #Retrieve information from the dictionary and reformats to output.
     print("First address:" + classes[network_class]["first_address"])
     print("Last address:" + classes[network_class]["last_address"])
 
 
-get_class_stats("200.206.18.7")
+#get_class_stats("200.206.18.7")
+#get_class_stats("136.206.18.7")
+#get_class_stats("240.192.16.5")
